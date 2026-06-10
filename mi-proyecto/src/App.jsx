3f8c42735c478d749af63cb9cc1ce6e18b56486c
juanpaa777 <juandiegopardozamrripa@gmail.com>
2026-06-10 16:41:51 -0600

    primer commit

---
+++ mi-proyecto/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif',
+      background: '#f0f4ff'
+    }}>
+      <h1>¡Bienvenido! 👋</h1>
+      <p>Mi primer despliegue en Vercel</p>
+    </div>
+  )
+}
+
+export default App
